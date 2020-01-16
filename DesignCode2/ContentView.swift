9428ed118cacbc690c6863d3a32a038d43425911
Christopher Davis <chris@christopheralandavis.com>
2020-01-15 19:26:03 -0500

    Initial Commit

---
+++ DesignCode2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode2
+//
+//  Created by Christopher Davis on 1/15/20.
+//  Copyright © 2020 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
