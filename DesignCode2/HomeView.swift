54eb1219046cea800688fa20504262dc82bb0254
Christopher Davis <chris@christopheralandavis.com>
2020-01-18 16:12:58 -0500

    Initial Commit

---
+++ DesignCode2/HomeView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeView.swift
+//  DesignCode2
+//
+//  Created by Christopher Davis on 1/18/20.
+//  Copyright © 2020 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                Spacer()
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
