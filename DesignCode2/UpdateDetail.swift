fc1e541da57119bf5a20181cc085bbee2e7209dd
Christopher Davis <chris@christopheralandavis.com>
2020-01-20 14:34:54 -0500

    Set up the app to pass data between the update view and the detail view. Debugged some spacing on the list in the update detail view

---
+++ DesignCode2/UpdateDetail.swift
@@ -0,0 +1,35 @@
+//
+//  UpdateDetail.swift
+//  DesignCode2
+//
+//  Created by Christopher Davis on 1/20/20.
+//  Copyright © 2020 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    
+    var update: Update = updateData[2]
+    
+    var body: some View {
+        List {
+            VStack {
+                Image(update.image)
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .aspectRatio(contentMode: .fit)
+                Text(update.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .navigationBarTitle(update.title)
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
