66da875c7d907cbde6e38ab8f52633075a98f0d8
Christopher Davis <chris@christopheralandavis.com>
2020-01-27 13:58:37 -0500

    Built the progress ring Set it so it can scale depending on the size passed to it

---
+++ DesignCode2/RingView.swift
@@ -0,0 +1,52 @@
+//
+//  RingView.swift
+//  DesignCode2
+//
+//  Created by Christopher Davis on 1/27/20.
+//  Copyright © 2020 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    
+    var color1 = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    var color2 = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent: CGFloat = 88
+    
+    var body: some View {
+        
+        let multiplier = width / 44
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
+            
+            Circle()
+                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+                .frame(width: width, height: height)
+            
+            Circle()
+                .trim(from: progress, to: 1)
+                .stroke(
+                    LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 0)
+            )
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1.0, y: 0, z: 0))
+                .frame(width: width, height: height)
+                .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
+            
+            Text("\(Int(percent))%")
+            .font(.system(size: 14 * multiplier))
+            .fontWeight(.bold)
+        }
+    }
+}
+
+struct RingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RingView()
+    }
+}
