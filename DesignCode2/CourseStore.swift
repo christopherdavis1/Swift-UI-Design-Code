691746318668bba24859dc6dd10bb90a0793a8f4
Christopher Davis <chris@christopheralandavis.com>
2020-02-08 15:07:57 -0500

    Pulling images from the web

--- DesignCode2/CourseStore.swift
+++ DesignCode2/CourseStore.swift
@@ -32,14 +32,17 @@ class CourseStore: ObservableObject {
     @Published var courses: [Course] = courseData
     
     init() {
+        
+        let color = [#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)]
+        
         getArray(id: "course") { (items) in
             items.forEach { (item) in
                 self.courses.append(Course(
                     title: item.fields["title"] as! String,
                     subtitle: item.fields["subtitle"] as! String,
-                    image: #imageLiteral(resourceName: "Background1"),
+                    image: item.fields.linkedAsset(at: "image")?.url ?? URL(string: "")!,
                     logo: #imageLiteral(resourceName: "Logo1"),
-                    color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                    color: color.randomElement()!,
                     show: false))
             }
         }
